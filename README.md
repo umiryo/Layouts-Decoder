854a041a29bcb7596c6dd17df15ed9d37b18ebb6
zenith-chan <ddbdhd1312@gmail.com>
2024-05-24 18:54:03 +0300

    Added ru/en decode, 60% of code rewritten

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Layouts-Decoder
 <img src="./screenshot.png">
-simple decoder from english keyboard layout to russian keyboard layout (from russian to english works incorrrectly yet, wait for updates or fork this repo)
+#### simple decoder from english keyboard layout to russian keyboard layout and back
+# Why do you need that Decoder?
+#### for example, your stupid friend wanted to text you something important but they FORGOT to change the keyboard layout. What will you do? Ask them to text you again? hell naw, you'd better use THIS decoder and make those nonsense words understandable!
+# Usage
+#### simply run .exe(Windows) or binary file on your pc
--- engru.c
+++ engru.c
@@ -2,125 +2,115 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <wchar.h>
 
-void en_ru_decode(char **english_alphabet, char **russian_alphabet, char *string) {
-    size_t length = strlen(string);
+void en_ru_decode(const wchar_t *english_alphabet, const wchar_t *russian_alphabet, const wchar_t *string) {
+    size_t length = wcslen(string);
+    wchar_t string_arr[length + 1]; 
 
-    char *space[1] = {" "};
-    char *numbers[10] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-
-
-    char *string_arr[length]; 
-
-    for (int i = 0; i < length; ++i) {
+    for (size_t i = 0; i < length; ++i) {
+        int found = 0;
         for (int j = 0; j < 84; ++j) {
-            if (string[i] == *english_alphabet[j]) {
+            if (string[i] == english_alphabet[j]) {
                 string_arr[i] = russian_alphabet[j];
-                break;
-            } else if (string[i] == *space[0]) {
-                string_arr[i] = space[0];
+                found = 1;
                 break;
             }
         }
-        for (int n = 0; n < 10; ++n) {
-            if (string[i] == *numbers[n]) {
-                string_arr[i] = numbers[n];
-                break;
-            }
+        if (!found) {
+            string_arr[i] = string[i];
         }
     }
+    string_arr[length] = L'\0';
 
-    for (int v = 0; v < length; ++v) {
-        printf("%s", string_arr[v]);
-    }
-    
+    wprintf(L"%ls", string_arr);
 }
 
-void ru_en_decode(char **english_alphabet, char **russian_alphabet, char *string) {
-    size_t length = strlen(string);
+void ru_en_decode(const wchar_t *english_alphabet, const wchar_t *russian_alphabet, const wchar_t *string) {
+    size_t length = wcslen(string);
+    wchar_t string_arr[length + 1]; 
 
-    char *space[1] = {" "};
-    char *numbers[10] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-
-
-    char *string_arr[length]; 
-
-    for (int i = 0; i < length; ++i) {
+    for (size_t i = 0; i < length; ++i) {
+        int found = 0;
         for (int j = 0; j < 84; ++j) {
-            if (string[i] == *russian_alphabet[j]) {
+            if (string[i] == russian_alphabet[j]) {
                 string_arr[i] = english_alphabet[j];
-                break;
-            } else if (string[i] == *space[0]) {
-                string_arr[i] = space[0];
+                found = 1;
                 break;
             }
         }
-        for (int n = 0; n < 10; ++n) {
-            if (string[i] == *numbers[n]) {
-                string_arr[i] = numbers[n];
-                break;
-            }
+        if (!found) {
+            string_arr[i] = string[i];
         }
     }
+    string_arr[length] = L'\0';
 
-    for (int v = 0; v < length; ++v) {
-        printf("%s", string_arr[v]);
-    }
-    
+    wprintf(L"%ls", string_arr);
 }
+
+int main() {
+    setlocale(LC_ALL, "");
+    wprintf(L"\tWelcome to en/ru decoder!\n");
+    wprintf(L" Warning: write '\\' before '\\' and '\"'\n");
+    wprintf(L"Enter your message: ");
+    
+    wchar_t main_string[100];
+    fgetws(main_string, 100, stdin);
+    main_string[wcslen(main_string) - 1] = L'\0';
 
 //@, #, $, ^, &, ~, `, |, <, > are not supported
-int main() {
-    printf("\tWelcome to en/ru decoder!\n");
-    printf(" Warning: write '\\' before '\\' and '\"'\n");
-    printf("Enter your message: ");
-    char main_string[100];
-    scanf(" %[^\n]s", main_string);
-
-
-	char *english_alphabet[84] = {
-        "F", "<", "D", "U", "L", "T", "~", ":", "P", "B",
-        "Q", "R", "K", "V", "Y", "J", "G", "H", "C", "N",
-        "E", "A", "{", "W", "X", "I", "O", "}", "S", "M",
-        "\"", ">", "Z",
-        "f", ",", "d", "u", "l", "t", "`", ";", "p", "b",
-        "q", "r", "k", "v", "y", "j", "g", "h", "c", "n",
-        "e", "a", "[", "w", "x", "i", "o", "]", "s", "m",
-        "'", ".", "z",
-        "/", "?", "-", "^", "$", "!", "@", "#", "%", "&", 
-        "*", "(", ")", "=", "+", "_", "|", "\\"        
+    wchar_t english_alphabet[84] = {
+        L'F', L'<', L'D', L'U', L'L', L'T', L'~', L':', L'P', L'B',
+        L'Q', L'R', L'K', L'V', L'Y', L'J', L'G', L'H', L'C', L'N',
+        L'E', L'A', L'{', L'W', L'X', L'I', L'O', L'}', L'S', L'M',
+        L'"', L'>', L'Z',
+        L'f', L',', L'd', L'u', L'l', L't', L'`', L';', L'p', L'b',
+        L'q', L'r', L'k', L'v', L'y', L'j', L'g', L'h', L'c', L'n',
+        L'e', L'a', L'[', L'w', L'x', L'i', L'o', L']', L's', L'm',
+        L'\'', L'.', L'z',
+        L'/', L'?', L'-', L'^', L'$', L'!', L'@', L'#', L'%', L'&', 
+        L'*', L'(', L')', L'=', L'+', L'_', L'|', L'\\'        
     };
 
-    char *russian_alphabet[84] = {
-        "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И",
-        "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т",
-        "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь",
-        "Э", "Ю", "Я",
-        "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и",
-        "й", "к", "л", "м", "н", "о", "п", "р", "с", "т",
-        "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь",
-        "э", "ю", "я",      
-        ".", ",", "-", ":", ";", "!", "\"", "№", "%", "?",
-        "*", "(", ")", "=", "+", "_", "/", "\\"
+    wchar_t russian_alphabet[84] = {
+        L'А', L'Б', L'В', L'Г', L'Д', L'Е', L'Ё', L'Ж', L'З', L'И',
+        L'Й', L'К', L'Л', L'М', L'Н', L'О', L'П', L'Р', L'С', L'Т',
+        L'У', L'Ф', L'Х', L'Ц', L'Ч', L'Ш', L'Щ', L'Ъ', L'Ы', L'Ь',
+        L'Э', L'Ю', L'Я',
+        L'а', L'б', L'в', L'г', L'д', L'е', L'ё', L'ж', L'з', L'и',
+        L'й', L'к', L'л', L'м', L'н', L'о', L'п', L'р', L'с', L'т',
+        L'у', L'ф', L'х', L'ц', L'ч', L'ш', L'щ', L'ъ', L'ы', L'ь',
+        L'э', L'ю', L'я',      
+        L'.', L',', L'-', L':', L';', L'!', L'\"', L'№', L'%', L'?',
+        L'*', L'(', L')', L'=', L'+', L'_', L'/', L'\\'
     };
-    printf(" Your message is: \"%s\"\n", main_string);
-    printf("Decoded message is: \"");
-    for (int i = 0; i < 84; ++i) {
-        char ch1 = *english_alphabet[i];
-        char ch2 = *russian_alphabet[i];
 
-        if(strchr(main_string, ch1)) {
-            en_ru_decode(english_alphabet, russian_alphabet, main_string);
-            break;
-        } else if(strchr(main_string, ch2)) {
-            printf("ru/en decode currently works incorrectly, wait for project update");
-            //ru_en_decode(english_alphabet, russian_alphabet, main_string);
+    wprintf(L" Your message is: \"%ls\"\n", main_string);
+    wprintf(L"Decoded message is: \"");
+
+    int found = 0;
+    for (size_t i = 0; i < wcslen(main_string); ++i) {
+        for (int j = 0; j < 84; ++j) {
+            if (main_string[i] == russian_alphabet[j]) {
+                ru_en_decode(english_alphabet, russian_alphabet, main_string);
+                found = 1;
+                break;
+            } else if (main_string[i] == english_alphabet[j]) {
+                en_ru_decode(english_alphabet, russian_alphabet, main_string);
+                found = 1;
+                break;
+            }
+        }
+        if (found) {
             break;
         }
     }
 
-    printf("\"\n");
-    getchar();
+    if (!found) {
+        wprintf(L"No valid characters found for decoding");
+    }
+    
+    wprintf(L"\"\n");
     getchar();
     return 0;
 }
